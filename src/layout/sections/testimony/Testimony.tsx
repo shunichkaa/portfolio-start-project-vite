9f51e108c0fe60bc50e9328e7bee01439e63afe4
Aleksandra Mate <matealeksandra@gmail.com>
2025-03-29 14:11:52 +0100

    add svg "quotes" created file Testimony

---
+++ src/layout/sections/testimony/Testimony.tsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+import {SectionTitle} from "../../../components/SectionTitle.tsx";
+import {Icon} from "../../../components/icon/Icon.tsx";
+
+
+export const Testimony = () => {
+    return (
+        <StyledTestimony>
+            <SectionTitle>Testimony</SectionTitle>
+            <Icon iconId={}/>
+
+        </StyledTestimony>
+    );
+};
+
+export const StyledTestimony = styled.section`
+
+`
