df97824f2297acaebc96dc66a4edb293f12b775f
Aleksandra Mate <matealeksandra@gmail.com>
2025-04-03 21:48:59 +0200

    small fix

--- src/components/icon/Icon.tsx
+++ src/components/icon/Icon.tsx
@@ -1,16 +1,17 @@
 import iconSprite from '../../assets/images/icon-sprite.svg'
 
 
-type IconPropsType = {
-    iconId: string
-    weight?: string
-    height?: string
-    viewBox?: string
-}
+export type IconPropsType = {
+    iconId: string;
+    width?: string;
+    height?: string;
+    viewBox?: string;
+};
+
 
 export const Icon = (props: IconPropsType) => {
     return (
-            <svg width={props.weight || "50"} height={props.height || "50"} viewBox={props.viewBox || "0 0 50 50" }fill="none" xmlns="http://www.w3.org/2000/svg">
+            <svg width={props.width|| "50"} height={props.height || "50"} viewBox={props.viewBox || "0 0 50 50" }fill="none" xmlns="http://www.w3.org/2000/svg">
                 <use xlinkHref={`${iconSprite}#${props.iconId}`}></use>
             </svg>
     );
