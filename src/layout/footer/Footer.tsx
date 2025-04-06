89dece2935a60c797d63ca8b6c6ab0e23d3c317e
Aleksandra Mate <matealeksandra@gmail.com>
2025-04-06 16:33:13 +0200

    add style for footer

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 import {Icon} from "../../components/icon/Icon.tsx";
 import {FlexWrapper} from "../../components/FlexWrapper.tsx";
+import {theme} from "../../styles/Theme.tsx";
+
 
 export const Footer = () => {
     return (
@@ -31,20 +33,45 @@ export const Footer = () => {
 };
 
 const StyledFooter = styled.footer`
-min-height: 20vh;
+    background-color: ${theme.colors.primaryBg};
+    padding: 40px 0;
 `
 const Name = styled.span`
+    font-family: Josefin Sans, sans-serif;
+    font-weight: 700;
+    font-size: 22px; 
+    letter-spacing: 3px;
 
 `
 const SocialList = styled.ul`
     display: flex;
-    gap: 30px;
+    gap: 20px;
+    margin: 30px 0;
     
 `
 const SocialLink = styled.a`
-
+    background-color: rgba(255, 255, 255, 0.1);
+    border-radius: 50%;
+    width: 35px;
+    height: 35px;
+    
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+    color: ${theme.colors.accent};
+    
+    &:hover{
+        color: ${theme.colors.primaryBg};
+        transform: translateY(-4px);
+    }
 `
 const CopyRight = styled.small`
+
+    font-weight: 400;
+    font-size: 12px;
+    text-align: center;
+    opacity: 0.5;
 
 `
 const SocialItem = styled.li`
