eb32effc61cbf7b30630b289cc1c56868844960c
Aleksandra Mate <matealeksandra@gmail.com>
2025-04-14 16:46:08 +0200

    refactoring section Slider

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -1,62 +1,23 @@
-import styled from "styled-components";
 import {FlexWrapper} from "../FlexWrapper.tsx";
-import {theme} from "../../styles/Theme.tsx";
+import { S } from "./Slider_Styles.ts";
+import React from "react";
 
-export const Slider = () => {
+
+export const Slider:React.FC = () => {
     return (
-        <StyledSlider>
+        <S.Slider>
             <FlexWrapper>
-                <Slide>
-                    <Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</Text>
-                    <Name>@ivan ivanow</Name>
-                </Slide>
+                <S.Slide>
+                    <S.Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</S.Text>
+                    <S.Name>@ivan ivanow</S.Name>
+                </S.Slide>
             </FlexWrapper>
-            <Pagination>
+            <S.Pagination>
                 <span></span>
                 <span className={"activ"}></span>
                 <span></span>
-            </Pagination>
-        </StyledSlider>
+            </S.Pagination>
+        </S.Slider>
     );
 };
 
-const StyledSlider = styled.div`
-    max-width: 500px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-`
-const Slide = styled.div`
-    text-align: center;
-`
-const Text = styled.p`
-
-`
-const Name  = styled.span`
-    font-family: Josefin Sans, sans-serif;
-    font-weight: 600;
-    font-size: 16px;
-    letter-spacing: 1px;
-    text-transform: uppercase;
-    margin: 22px 0 42px;
-    display: inline-block;
-
-`
-const Pagination  = styled.div`
-    span {
-        display: inline-block;
-        width: 7px;
-        height: 7px;
-        border-radius: 20px;
-        background: rgba(255, 255, 255, 0.5);
-        
-        & + span {
-            margin-left: 5px;
-        }
-        
-        &.activ {
-            background-color: ${theme.colors.accent};
-            width: 20px;
-        }
-    }
-`
---
+++ src/components/slider/Slider_Styles.ts
@@ -0,0 +1,55 @@
+import styled from "styled-components";
+import {theme} from "../../styles/Theme.tsx";
+
+const Slider = styled.div`
+    max-width: 500px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+const Slide = styled.div`
+    text-align: center;
+`
+
+const Text = styled.p`
+
+`
+
+const Name  = styled.span`
+    font-family: Josefin Sans, sans-serif;
+    font-weight: 600;
+    font-size: 16px;
+    letter-spacing: 1px;
+    text-transform: uppercase;
+    margin: 22px 0 42px;
+    display: inline-block;
+
+`
+
+const Pagination  = styled.div`
+    span {
+        display: inline-block;
+        width: 7px;
+        height: 7px;
+        border-radius: 20px;
+        background: rgba(255, 255, 255, 0.5);
+        
+        & + span {
+            margin-left: 5px;
+        }
+        
+        &.active {
+            background-color: ${theme.colors.accent};
+            width: 20px;
+        }
+    }
+`
+
+export const S = {
+    Slider,
+    Slide,
+    Text,
+    Name,
+    Pagination
+}
