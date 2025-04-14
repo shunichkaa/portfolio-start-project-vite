92021641e0457c9bec548130aeaf16b6bc3c2451
Aleksandra Mate <matealeksandra@gmail.com>
2025-04-14 16:19:35 +0200

    refactoring section Contacts

--- src/layout/sections/contact/Contact.tsx
+++ src/layout/sections/contact/Contact.tsx
@@ -1,63 +1,22 @@
-import styled from "styled-components";
 import {SectionTitle} from "../../../components/SectionTitle.tsx";
 import {Button} from "../../../components/Button.tsx";
 import {Container} from "../../../components/Container.tsx";
-import {theme} from "../../../styles/Theme.tsx";
+import { S } from "./Contacts_Styles.ts";
+
 
 
 export const Contact = () => {
     return (
-        <StyledContact>
+        <S.Contact>
             <Container>
                 <SectionTitle>Contact</SectionTitle>
-                <StyledForm>
-                    <Field placeholder={"name"}/>
-                    <Field placeholder={"subject"}/>
-                    <Field as={"textarea"} placeholder={"message"}/>
+                <S.Form>
+                    <S.Field placeholder={"name"}/>
+                    <S.Field placeholder={"subject"}/>
+                    <S.Field as={"textarea"} placeholder={"message"}/>
                     <Button type="submit">Send Message</Button>
-                </StyledForm>
+                </S.Form>
             </Container>
-        </StyledContact>
+        </S.Contact>
     );
 };
-
-const StyledContact= styled.section`
-
-`
-
-const StyledForm= styled.form`
-    max-width: 540px;
-    weight: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 16px;
-    margin: 0 auto;
-    
-    textarea {
-        resize: none;
-        height: 155px;
-    }
-`
-const Field= styled.input`
-    width: 100%;
-    border: 1px solid ${theme.colors.borderColor};
-    background: ${theme.colors.secondaryBg};
-    padding: 7px 15px;
-
-    font-family: Poppins, sans-serif;  
-    font-weight: 400;
-    font-size: 12px;
-    letter-spacing: 0.05rem;
-    color: ${theme.colors.font};
-    
-    &::placeholder {
-        color: ${theme.colors.placeholderColor};
-        text-transform: capitalize;
-    }
-    
-    &:focus-within {
-        outline: 1px solid ${theme.colors.borderColor};
-    }
-
-`
