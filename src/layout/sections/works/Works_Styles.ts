1e878fd46e9ea8fd4da6bfd9c4710d22b2eb2054
Aleksandra Mate <matealeksandra@gmail.com>
2025-04-15 12:14:30 +0200

    commit

--- src/layout/sections/works/Works_Styles.ts
+++ src/layout/sections/works/Works_Styles.ts
@@ -44,55 +44,55 @@ padding: 25px 20px;
 `
 
 const ImageWrapper = styled.div`
-  position: relative;
+    position: relative;
 
-  ${Button} {
-    opacity: 0;
-    position: absolute;
-    z-index: 2;
-      color: ${theme.colors.font};
-    left: 50%;
-    top: 50%;
-    transform: translate(-50%, -50%);
-
-  &::before {
-      width: 100%;
-      height: 100%;
-  }
-  }
-
-  &::before {
-      content: '';
-      position: absolute;
-      left: 0;
-      right: 0;
-      bottom: 0;
-      top: 0;
-      background: rgba(0, 0, 0, 0.3);
-      backdrop-filter: blur(4px);
-      opacity: 0;
-      z-index: 1;
-  }
-
-  &:hover {
     ${Button} {
-      opacity: 1;
+        opacity: 0;
+        position: absolute;
+        z-index: 2;
+        color: ${theme.colors.font};
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+
+        &::before {
+            width: 100%;
+            height: 100%;
+        }
     }
 
     &::before {
-        opacity: 1;
+        content: '';
+        position: absolute;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        top: 0;
+        background: rgba(0, 0, 0, 0.3);
+        backdrop-filter: blur(4px);
+        opacity: 0;
+        z-index: 1;
     }
-  }
-    
-  @media ${theme.media.tablet} {
-      ${Button} {
-          opacity: 1;
-      }
 
-      &::before {
-          opacity: 1;
-      }
-  }
+    &:hover {
+        ${Button} {
+            opacity: 1;
+        }
+
+        &::before {
+            opacity: 1;
+        }
+    }
+
+    @media ${theme.media.tablet} {
+        ${Button} {
+            opacity: 1;
+        }
+
+        &::before {
+            opacity: 1;
+        }
+    }
 `
 
 export const S = {
