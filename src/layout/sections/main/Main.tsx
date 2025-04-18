b8a61021fe99e64480d0bd9235d8c71481ef3834
Aleksandra Mate <matealeksandra@gmail.com>
2025-04-18 10:46:50 +0200

    add a Typewriter effect

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -3,6 +3,7 @@ import {FlexWrapper} from "../../../components/FlexWrapper.ts";
 import {Container} from "../../../components/Container.ts";
 import React from "react";
 import { S } from './Main_Styles.ts';
+import Typewriter from 'typewriter-effect';
 
 export const Main:React.FC = () => {
     return (
@@ -12,7 +13,18 @@ export const Main:React.FC = () => {
                     <div>
                     <S.SmallText>Hi There</S.SmallText>
                     <S.Name>I'm <span>Aleksandra Mate</span></S.Name>
-                    <S.MainTitle>A Web Developer</S.MainTitle>
+                    {/*<S.MainTitle>A Web Developer</S.MainTitle>*/}
+                    <S.MainTitle>
+                        <p>A Web Developer</p>
+                        <Typewriter
+                            options={{
+                                strings: ['A Web Developer','A Web Designer','A Front-End Developer',],
+                                autoStart: true,
+                                loop: true,
+                                delay: 150,
+                            }}
+                        />
+                    </S.MainTitle>
                     </div>
 
                     <S.PhotoWrapper>
--- src/layout/sections/main/Main_Styles.ts
+++ src/layout/sections/main/Main_Styles.ts
@@ -51,7 +51,11 @@ const Photo = styled.img`
 `
 
 const MainTitle= styled.h1`
-    ${font({ weight: 400, Fmax: 27, Fmin: 20})}
+    ${font({ weight: 400, Fmax: 27, Fmin: 20})};
+    
+    p {
+        display: none;
+    }
 
 `
 
